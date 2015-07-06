4b8b7273a42d19444fa56e804595064b818e2baa
Zach Archer <zkarcher@gmail.com>
2015-07-06 13:59:00 -0700

    nice rotors

--- html/js/classes/Copter.js
+++ html/js/classes/Copter.js
@@ -1,21 +1,36 @@
 var copterGeom = null;
-var material = null;
+var rotorGeom = null;
 
 const TORUS_RADIUS = 10.0;
 const TUBE_THICKNESS = 0.4;
 const TUBE_SEGMENTS = 6;
 const COPTER_Z = -40;
+const ROTOR_RADIUS = 4.0;
+const ROTOR_PARTIAL_ALPHA = 0.3;
 
 function Copter( scene ){
 	var self = this;
 
+	var rotors = [];
+	var rotorSpeeds = new Float32Array( 4 );
+	for( var i=0; i<4; i++ ) {
+		rotorSpeeds[i] = randRange( 0.5, 1.0 );
+	}
+
 	var spr;
 
 	function initGeom(){
+		spr = new THREE.Group;
+		spr.position.z = COPTER_Z;
+		scene.add( spr );
+
+		var material = new THREE.MeshBasicMaterial({ color:0x008888 });
+		var materialAlpha = new THREE.MeshBasicMaterial({ color:0xff0000, opacity:ROTOR_PARTIAL_ALPHA, transparent:true });
+
 		// Shared geometry, reusable by each Copter
 		if( !copterGeom ) {
 			// radius, tube diam, radial segments, tubular segments
-			var copterGeom = new THREE.TorusGeometry( TORUS_RADIUS, TUBE_THICKNESS, TUBE_SEGMENTS, 100 );
+			var copterGeom = new THREE.TorusGeometry( TORUS_RADIUS, TUBE_THICKNESS, TUBE_SEGMENTS, 50 );
 
 			var peaceArmTopBottomGeom = new THREE.CylinderGeometry( TUBE_THICKNESS, TUBE_THICKNESS, TORUS_RADIUS*2, TUBE_SEGMENTS, 1, false );
 			copterGeom.merge( peaceArmTopBottomGeom );
@@ -33,12 +48,45 @@ function Copter( scene ){
 				copterGeom.merge( armGeom, rotMatrix );
 			}
 
-			material = new THREE.MeshBasicMaterial({ color:0x008888, wireframe:true });
-		}
+			// Central box (battery compartment)
+			var boxGeom = new THREE.BoxGeometry( 5, 3, 2 );
+			var boxMatrix = new THREE.Matrix4().makeTranslation( 0, 0, 1 );
+			copterGeom.merge( boxGeom, boxMatrix );
 
-		spr = new THREE.Group;
-		spr.position.z = COPTER_Z;
-		scene.add( spr );
+			// Four spheres where the rotors will be
+			for( var i=0; i<4; i++ ) {
+				var SPHERE_RADIUS = 1.2;
+				var sphereGeom = new THREE.SphereGeometry( SPHERE_RADIUS, 6, 6 );
+				var angle = ((i+0.5)/2.0) * Math.PI;
+				var loc = new THREE.Vector3(
+					Math.cos(angle)*TORUS_RADIUS*1.07,
+					Math.sin(angle)*TORUS_RADIUS,
+					-0.5
+				);
+				var sphereMatrix = new THREE.Matrix4().makeTranslation( loc.x, loc.y, loc.z );
+
+				copterGeom.merge( sphereGeom, sphereMatrix );
+
+				// Create a rotor at this location
+				var rotor = new THREE.Group();
+				rotor.position.set( loc.x, loc.y, loc.z - 1.5 );
+				spr.add( rotor );
+				rotors.push( rotor );
+
+				if( !rotorGeom ) {
+					rotorGeom = new THREE.CircleGeometry( ROTOR_RADIUS, 8, 0, Math.PI*0.5 );
+					rotorGeom.merge( new THREE.CircleGeometry( ROTOR_RADIUS, 8, Math.PI, Math.PI*0.5 ) );
+				}
+
+				var blades0 = new THREE.Mesh( rotorGeom, material );
+				var blades1 = new THREE.Mesh( rotorGeom, materialAlpha );
+				blades1.rotation.z = Math.PI*0.5;
+
+				rotor.add( blades0 );
+				rotor.add( blades1 );
+			}
+
+		}
 
 		var copter = new THREE.Mesh( copterGeom, material );
 		spr.add( copter );
@@ -48,5 +96,9 @@ function Copter( scene ){
 	self.perFrame = function( timeMult ) {
 		spr.rotation.x += 0.01 * timeMult;
 		spr.rotation.y += 0.02 * timeMult;
+
+		for( var i=0; i<4; i++ ) {
+			rotors[i].rotation.z += rotorSpeeds[i]*timeMult;
+		}
 	}
 }
