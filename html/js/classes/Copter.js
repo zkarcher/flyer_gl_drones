e2998d753d0434d3511d71e2c95d7c310caf7de1
Zach Archer <zkarcher@gmail.com>
2015-07-06 13:31:04 -0700

    peace sign arms

--- html/js/classes/Copter.js
+++ html/js/classes/Copter.js
@@ -1,6 +1,9 @@
-var torusGeom = null;
+var copterGeom = null;
 var material = null;
 
+const TORUS_RADIUS = 10.0;
+const TUBE_THICKNESS = 0.4;
+const TUBE_SEGMENTS = 6;
 const COPTER_Z = -40;
 
 function Copter( scene ){
@@ -10,9 +13,26 @@ function Copter( scene ){
 
 	function initGeom(){
 		// Shared geometry, reusable by each Copter
-		if( !torusGeom ) {
+		if( !copterGeom ) {
 			// radius, tube diam, radial segments, tubular segments
-			torusGeom = new THREE.TorusGeometry( 10, 3, 16, 100 );
+			var copterGeom = new THREE.TorusGeometry( TORUS_RADIUS, TUBE_THICKNESS, TUBE_SEGMENTS, 100 );
+
+			var peaceArmTopBottomGeom = new THREE.CylinderGeometry( TUBE_THICKNESS, TUBE_THICKNESS, TORUS_RADIUS*2, TUBE_SEGMENTS, 1, false );
+			copterGeom.merge( peaceArmTopBottomGeom );
+
+			// 2 more peace arms: left, right
+			for( var i=-1; i<=1; i+=2 ) {
+				var armGeom = new THREE.CylinderGeometry( TUBE_THICKNESS, TUBE_THICKNESS, TORUS_RADIUS, TUBE_SEGMENTS, 1, false );
+
+				var matrix = new THREE.Matrix4();
+				matrix.makeTranslation( 0, TORUS_RADIUS/2, 0 );
+
+				var rotMatrix = new THREE.Matrix4().makeRotationZ( (1.0+i*0.333)*Math.PI );
+				rotMatrix.multiply( matrix );
+
+				copterGeom.merge( armGeom, rotMatrix );
+			}
+
 			material = new THREE.MeshBasicMaterial({ color:0x008888, wireframe:true });
 		}
 
@@ -20,8 +40,8 @@ function Copter( scene ){
 		spr.position.z = COPTER_Z;
 		scene.add( spr );
 
-		var torus = new THREE.Mesh( torusGeom, material );
-		spr.add( torus );
+		var copter = new THREE.Mesh( copterGeom, material );
+		spr.add( copter );
 	}
 	initGeom();
 
